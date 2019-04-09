7b89bdea52ca258125c6138beac101bf28ff4b1b
Jhalaa <jhalaachinoy@gmail.com>
2019-04-09 12:19:33 -0700

    Initial Commit

---
+++ rateUsIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rateUsIOS
+//
+//  Created by Jhalaa Chinoy Tejasvi on 4/9/19.
+//  Copyright © 2019 Jhalaa Chinoy Tejasvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
