e62f3156563c88e1f9ccf93bf220ae1a00e458a5
Jhalaa <jhalaachinoy@gmail.com>
2019-04-09 21:31:40 -0700

    Add login storyboard and controllrs

---
+++ rateUsIOS/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  rateUsIOS
+//
+//  Created by Jhalaa Chinoy Tejasvi on 4/9/19.
+//  Copyright © 2019 Jhalaa Chinoy Tejasvi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
